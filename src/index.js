b5cd2be79f3b4bf059c7785a80d48a269d39797f
ngure1 <ngugijoseph700@gmail.com>
2024-07-20 18:14:49 +0300

    feat :sparkles: : add cookieParser

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-import e from "express";
+import express from "express";
 import "dotenv/config";
 import { connectDb } from "./db/index.js";
-import errorHandler from "./middlewear/errorHandler.js";
-// import routes
+import errorHandler from "./middleware/errorHandler.js";
+import cookieParser from "cookie-parser";
+
+// Import routes
 import contactRoutes from "./routes/contactRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
-const app = e();
+const app = express();
 const port = process.env.PORT;
 
-// connect to the database
+// Connect to the database
 connectDb().then(() => {
-	//common middlewear
-	app.use(e.json());
+	// Common middleware
+	app.use(express.json());
+	app.use(cookieParser());
 
-	app.listen(port, () => {
-		console.log(`Server running on port ${port} ...`);
-	});
-
-	//routes
+	// Routes
 	app.use("/api/contacts", contactRoutes);
 	app.use("/auth/users", userRoutes);
 
-	//error handling middlewear
+	// Error handling middleware
 	app.use(errorHandler);
+
+	// Start the server
+	app.listen(port, () => {
+		console.log(`Server running on port ${port} ...`);
+	});
 });
