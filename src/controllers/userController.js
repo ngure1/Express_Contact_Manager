d9ff30ac0c6af0207d6ed436a5b6933275b0dce4
ngure1 <ngugijoseph700@gmail.com>
2024-07-20 11:49:16 +0300

    feat :sparkles: : add user registration functionality

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,62 @@
 import expressAsyncHandler from "express-async-handler";
+import { User } from "../models/userModel.js";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+
+const userRegisterSchema = z.object({
+	firstName: z.string({
+		required_error: "First Name is required",
+	}),
+	lastName: z.string({
+		required_error: "Last Name is required",
+	}),
+	username: z.string().optional(),
+	email: z
+		.string({
+			required_error: "Email is required",
+		})
+		.email("Enter a valid email"),
+	password: z
+		.string({
+			required_error: "Password is required",
+		})
+		.min(8, {
+			message: "Password must be a minimum of 8 characters",
+		}),
+});
 
 const usersRegister = expressAsyncHandler(async (req, res) => {
-	res.json({
-		message: "User registration endpoint",
+	const result = userRegisterSchema.safeParse(req.body);
+	if (!result.success) {
+		const errorMessages = result.error.errors.map((e) => e.message).join(", ");
+		res.status(400);
+		throw new Error(errorMessages);
+	}
+
+	const { email, password } = result.data;
+	const existingUser = await User.findOne({ email });
+	if (existingUser) {
+		res.status(400);
+		throw new Error("User with this email already exists");
+	}
+
+	const hashedPassword = await bcrypt.hash(password, 10);
+	const user = await User.create({
+		password: hashedPassword,
+		...result.data,
+	});
+	if (!user) {
+		throw new Error("User not created");
+	}
+
+	res.status(201).json({
+		id: user.id,
+		firstName: user.firstName,
+		lastName: user.lastName,
+		username: user.username,
+		email: user.email,
+		createdAt: user.createdAt,
+		updatedAt: user.updatedAt,
 	});
 });
 
